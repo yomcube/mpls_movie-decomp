fd8a5c2e1063a11c04d501ee6963b462a4974dd4
RootCubed <liam@rootcubed.dev>
2025-05-21 14:49:20 +0200

    Add initial setup for the project

--- configure.py
+++ configure.py
@@ -29,7 +29,7 @@ from tools.project import (
 # Game versions
 DEFAULT_VERSION = 0
 VERSIONS = [
-    "GAMEID",  # 0
+    "MPLS01",  # 0
 ]
 
 parser = argparse.ArgumentParser()
@@ -226,14 +226,14 @@ cflags_rel = [
     "-sdata2 0",
 ]
 
-config.linker_version = "GC/1.3.2"
+config.linker_version = "Wii/1.0"
 
 
 # Helper function for Dolphin libraries
 def DolphinLib(lib_name: str, objects: List[Object]) -> Dict[str, Any]:
     return {
         "lib": lib_name,
-        "mw_version": "GC/1.2.5n",
+        "mw_version": "Wii/1.0",
         "cflags": cflags_base,
         "progress_category": "sdk",
         "objects": objects,
@@ -244,7 +244,7 @@ def DolphinLib(lib_name: str, objects: List[Object]) -> Dict[str, Any]:
 def Rel(lib_name: str, objects: List[Object]) -> Dict[str, Any]:
     return {
         "lib": lib_name,
-        "mw_version": "GC/1.3.2",
+        "mw_version": "Wii/1.0",
         "cflags": cflags_rel,
         "progress_category": "game",
         "objects": objects,

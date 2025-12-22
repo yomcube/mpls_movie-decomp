44c1433c1a72df8eb17ef1998745f0b6b7573d94
TomCube <121310847+yomcube@users.noreply.github.com>
2025-12-21 23:47:17 -0600

    dScStage::execute()

--- src/REL/d_s_stageNP/d_s_stage.cpp
+++ src/REL/d_s_stageNP/d_s_stage.cpp
@@ -2,10 +2,21 @@
 #include <game/framework/f_base.hpp>
 #include <game/framework/f_profile.hpp>
 
+extern void fn_8000C4B0();
+extern void fn_8000C500();
+extern void fn_8000B680();
+
 BASE_PROFILE(STAGE, dScStage_c);
 
+int dScStage_c::execute() {
+    fn_8000C4B0();
+    fn_8000C500();
+    fn_8000B680();
+    return SUCCEEDED;
+}
+
 int dScStage_c::draw() {
-    return fBase_c::SUCCEEDED;
+    return SUCCEEDED;
 }
 
 void dScStage_c::deleteReady() {}
